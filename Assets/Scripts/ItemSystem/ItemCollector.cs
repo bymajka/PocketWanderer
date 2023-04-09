598e815387f1e6dd2ec5f57d4742c475d8c3c1fa
Kyryl Vashchuk <kirilvashchuk@gmail.com>
2023-04-09 19:31:01 +0300

    added item collector

---
+++ Assets/Scripts/ItemSystem/ItemCollector.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace ItemSystem
+{
+    public class ItemCollector : MonoBehaviour
+    {
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            ICollectible collectible = collision.GetComponent<ICollectible>();
+            if (collectible != null)
+            {
+                collectible.Collect();
+            }
+        }
+    }
+}
